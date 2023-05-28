4e3d802d0c7a56483c2c5743e21f8fff9ea1d837
Mohsin baig <Mohsincool1121@gmail.com>
2023-05-28 14:55:40 +0500

    'task'

---
+++ app.js
@@ -0,0 +1,51 @@
+// Task 01
+
+let number = (1,2,3,4,5,6,7,8,9,88,42)
+
+function Getevenadded (number){
+    let sum = 0;
+    
+
+    for (let i = 0; i < number.length; i++) {
+        
+        if(number[i] % 2 === 0)
+        sum = number[i]
+        
+
+
+
+       
+        
+         
+        
+        
+    }
+    
+    console.log (sum)
+
+
+    
+}
+Getevenadded(number)
+// TASK 02
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+
+//   Task 04
+
+let alpha = ("a","c","i","b","d","h","f","e")
+
+alpha.sort();
+    
+
+  
