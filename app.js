fafd24fb968c8fd6467fc90bb3b6f0b768c6756e
Mohsin baig <Mohsincool1121@gmail.com>
2023-05-30 22:23:30 +0500

    completed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Task 01
 
-let number = (1,2,3,4,5,6,7,8,9,88,42)
+let number = [1,2,3,4,5,6,7,8,9,88,42]
 
 function Getevenadded (number){
     let sum = 0;
@@ -43,9 +43,52 @@ for (let i = 1; i <= 100; i++) {
 
 //   Task 04
 
-let alpha = ("a","c","i","b","d","h","f","e")
+let alpha = ["a","c","i","b","d","h","f","e"]
 
 alpha.sort();
     
 
+// task 04
+
+
+
+
+
+
+// function carmodel1(Carmodel) {
+ 
+//   for (let car in Carmodel) {
+//     if (carmodel.hasOwnProperty(Carmodel, car)) {
+//       console.log(car+ ":" + Carmodel[car])
+      
+//     }
+//   }
   
+// }
+
+// let Carmodel ={
+//   make : "Toyota",
+//   Model : "Supra",
+//   year : 2009,
+//   price : 10000000
+// }
+
+// carmodel1(Carmodel);
+  
+function displayCarInfo(car) {
+  for (let property in car) {
+    if (car.hasOwnProperty(property)) {
+      console.log(property + ": " + car[property]);
+    }
+  }
+}
+
+let car = {
+  make: "Toyota",
+  model: "Supra",
+  year: 2009,
+  price: 10000000
+};
+
+displayCarInfo(car);
+
